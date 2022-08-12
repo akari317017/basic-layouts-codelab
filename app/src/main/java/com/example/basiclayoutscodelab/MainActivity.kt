09b703a4d7e19cb703c1c08fd9de15e51f5ec352
kanaya.akari <akari317017@gmail.com>
2022-08-12 21:13:38 +0900

    feat:SimpleListをImageScrollListに変更

--- app/src/main/java/com/example/basiclayoutscodelab/MainActivity.kt
+++ app/src/main/java/com/example/basiclayoutscodelab/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.basiclayoutscodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
@@ -15,13 +16,16 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Pets
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
 import com.example.basiclayoutscodelab.ui.theme.BasicLayoutsCodelabTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,7 +39,7 @@ class MainActivity : ComponentActivity() {
                 ) {
 //                    PhotographerCard()
 //                    LayoutsCodeLab()
-                    SimpleList()
+                    ImageList()
                 }
             }
         }
@@ -43,15 +47,53 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun SimpleList() {
+fun ImageListItem(index: Int) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Image(
+            painter = rememberImagePainter(
+                data = "https://developer.android.com/images/brand/Android_Robot.png"
+            ),
+            contentDescription = "AndroidLogo",
+            modifier = Modifier.size(50.dp)
+        )
+        Spacer(Modifier.width(10.dp))
+        Text("Item #$index", style = MaterialTheme.typography.subtitle1)
+    }
+}
+
+@Composable
+fun ImageList() {
+    val listSize = 100
     //スクロール位置を保存
     //リストをスクロールするために使用
     val scrollState = rememberLazyListState()
+    val coroutineScope = rememberCoroutineScope()
 
-    //composeのlazyColumnはRecyclerViewと同等
-    LazyColumn(state = scrollState) {
-        items(100) {
-            Text("Item #$it")
+    Column {
+        Row {
+            Button(onClick = {
+                coroutineScope.launch {
+                    scrollState.animateScrollToItem(0)
+                }
+            }) {
+                Text(text = "Scroll to the top")
+            }
+
+            Button(onClick = {
+                coroutineScope.launch {
+                    scrollState.animateScrollToItem(listSize - 1)
+                }
+            }) {
+                Text(text = "Scroll to the end")
+            }
+        }
+
+        //composeのlazyColumnはRecyclerViewと同等
+        LazyColumn(state = scrollState) {
+            items(100) {
+//            Text("Item #$it")
+                ImageListItem(index = it)
+            }
         }
     }
 }
